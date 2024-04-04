b030497afebc3393ec6e807e1b33ca5545ca795a
lenhutminhdang <ginoace2k1@gmail.com>
2024-04-04 11:24:08 +0700

    replace cors() to res.header 2

--- airbnb-server/app.js
+++ airbnb-server/app.js
@@ -28,6 +28,21 @@ app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+
+  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
   next();
 });
 
