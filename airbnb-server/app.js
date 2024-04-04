a65beb2a1035bd77fef1d5650b34d69cc80f9c46
lenhutminhdang <ginoace2k1@gmail.com>
2024-04-04 11:19:21 +0700

    replace cors() to res.header

--- airbnb-server/app.js
+++ airbnb-server/app.js
@@ -26,21 +26,8 @@ app.use(cookieParser());
 //   })
 // );
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
   next();
 });
 
