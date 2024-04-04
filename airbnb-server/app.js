9cb7aea7c0a3e9923d3eefc13f27939b7ed5fea3
lenhutminhdang <ginoace2k1@gmail.com>
2024-04-04 11:36:35 +0700

    replace cors() to res.header 3

--- airbnb-server/app.js
+++ airbnb-server/app.js
@@ -26,25 +26,25 @@ app.use(cookieParser());
 //   })
 // );
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
 
-  res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
+//   res.setHeader("Access-Control-Allow-Origin", process.env.ORIGIN);
 
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Credentials", true);
 
-  next();
-});
+//   next();
+// });
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -58,68 +58,101 @@ function getUserDataFromReq(req) {
   });
 }
 
-app.get("/", (req, res) => {
-  res.json("get /");
-});
-
-app.post("/signup", async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const userDoc = await User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcryptSalt),
-    });
-    res.json(userDoc);
-  } catch (error) {
-    res.status(422).json(error);
+app.get(
+  "/",
+  cors({
+    origin: process.env.ORIGIN,
+  }),
+  (req, res) => {
+    res.json("get /");
   }
-});
+);
 
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
+app.post(
+  "/signup",
+  cors({
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const { name, email, password } = req.body;
 
-  const userDoc = await User.findOne({ email });
-
-  if (userDoc) {
-    const passwordOK = bcrypt.compareSync(password, userDoc.password);
-    if (passwordOK) {
-      jwt.sign(
-        { email: userDoc.email, id: userDoc._id },
-        jwtSecret,
-        {},
-        (err, token) => {
-          if (err) throw err;
-          res.cookie("token", token).json(userDoc);
-        }
-      );
-    } else {
-      res.status(422).json("password not ok");
+    try {
+      const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt),
+      });
+      res.json(userDoc);
+    } catch (error) {
+      res.status(422).json(error);
     }
-  } else {
-    res.json("not found user");
   }
-});
+);
 
-app.get("/profile", (req, res) => {
-  const { token } = req.cookies;
-  if (token) {
-    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) throw err;
+app.post(
+  "/login",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const { email, password } = req.body;
 
-      const { name, email, _id } = await User.findById(userData.id);
+    const userDoc = await User.findOne({ email });
 
-      res.json({ name, email, _id });
-    });
-  } else {
-    res.json(null);
+    if (userDoc) {
+      const passwordOK = bcrypt.compareSync(password, userDoc.password);
+      if (passwordOK) {
+        jwt.sign(
+          { email: userDoc.email, id: userDoc._id },
+          jwtSecret,
+          {},
+          (err, token) => {
+            if (err) throw err;
+            res.cookie("token", token).json(userDoc);
+          }
+        );
+      } else {
+        res.status(422).json("password not ok");
+      }
+    } else {
+      res.json("not found user");
+    }
   }
-});
+);
 
-app.get("/logout", (req, res) => {
-  res.cookie("token", "").json(true);
-});
+app.get(
+  "/profile",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  (req, res) => {
+    const { token } = req.cookies;
+    if (token) {
+      jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+
+        const { name, email, _id } = await User.findById(userData.id);
+
+        res.json({ name, email, _id });
+      });
+    } else {
+      res.json(null);
+    }
+  }
+);
+
+app.get(
+  "/logout",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  (req, res) => {
+    res.cookie("token", "").json(true);
+  }
+);
 
 app.post("/places", (req, res) => {
   const { token } = req.cookies;
@@ -158,98 +191,138 @@ app.post("/places", (req, res) => {
   });
 });
 
-app.get("/user-places", (req, res) => {
-  const { token } = req.cookies;
+app.get(
+  "/user-places",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  (req, res) => {
+    const { token } = req.cookies;
 
-  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    if (err) throw err;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
 
-    const placesDoc = await Place.find({
-      owner: userData.id,
-    });
-
-    res.json(placesDoc);
-  });
-});
-
-app.get("/places/:placeId", async (req, res) => {
-  const { placeId } = req.params;
-  res.json(await Place.findById(placeId));
-});
-
-app.put("/places", async (req, res) => {
-  const { token } = req.cookies;
-  const {
-    placeId,
-    title,
-    address,
-    photos,
-    description,
-    perks,
-    extraInfo,
-    checkIn,
-    checkOut,
-    maxGuests,
-    price,
-  } = req.body;
-
-  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    if (err) throw err;
-
-    const placeDoc = await Place.findById(placeId);
-    if (userData.id === placeDoc.owner.toString()) {
-      placeDoc.set({
-        title,
-        address,
-        photos,
-        description,
-        perks,
-        extraInfo,
-        checkIn,
-        checkOut,
-        maxGuests,
-        price,
+      const placesDoc = await Place.find({
+        owner: userData.id,
       });
-      placeDoc.save();
-      res.json("ok");
-    }
-  });
-});
 
-app.get("/places", async (req, res) => {
-  res.json(await Place.find());
-});
-
-app.post("/bookings", async (req, res) => {
-  const { place, checkIn, checkOut, name, phone, price } = req.body;
-
-  const userData = await getUserDataFromReq(req);
-
-  Booking.create({
-    owner: userData.id,
-    place,
-    checkIn,
-    checkOut,
-    name,
-    phone,
-    price,
-  })
-    .then((doc) => {
-      res.json(doc);
-    })
-    .catch((err) => {
-      res.json(err);
+      res.json(placesDoc);
     });
-});
+  }
+);
 
-app.get("/bookings", async (req, res) => {
-  const userData = await getUserDataFromReq(req);
+app.get(
+  "/places/:placeId",
+  cors({
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const { placeId } = req.params;
+    res.json(await Place.findById(placeId));
+  }
+);
 
-  res.json(
-    await Booking.find({
+app.put(
+  "/places",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const { token } = req.cookies;
+    const {
+      placeId,
+      title,
+      address,
+      photos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    } = req.body;
+
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+
+      const placeDoc = await Place.findById(placeId);
+      if (userData.id === placeDoc.owner.toString()) {
+        placeDoc.set({
+          title,
+          address,
+          photos,
+          description,
+          perks,
+          extraInfo,
+          checkIn,
+          checkOut,
+          maxGuests,
+          price,
+        });
+        placeDoc.save();
+        res.json("ok");
+      }
+    });
+  }
+);
+
+app.get(
+  "/places",
+  cors({
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    res.json(await Place.find());
+  }
+);
+
+app.post(
+  "/bookings",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const { place, checkIn, checkOut, name, phone, price } = req.body;
+
+    const userData = await getUserDataFromReq(req);
+
+    Booking.create({
       owner: userData.id,
-    }).populate("place")
-  );
-});
+      place,
+      checkIn,
+      checkOut,
+      name,
+      phone,
+      price,
+    })
+      .then((doc) => {
+        res.json(doc);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  }
+);
+
+app.get(
+  "/bookings",
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  }),
+  async (req, res) => {
+    const userData = await getUserDataFromReq(req);
+
+    res.json(
+      await Booking.find({
+        owner: userData.id,
+      }).populate("place")
+    );
+  }
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
